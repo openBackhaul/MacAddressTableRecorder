6d8cf05ec6d258a072676dfb2154cb475bfa4791
Valentina Schiavi <valentina.schiavi@siaemic.com>
2023-10-02 09:34:39 +0200

    modified:   server/database/load.json modified:   server/index.js new file:   server/service/individualServices/ElasticsearchPreparation.js

--- server/index.js
+++ server/index.js
@@ -4,28 +4,37 @@ var path = require('path');
 var http = require('http');
 
 var oas3Tools = require('openbackhaul-oas3-tools');
+//var oas3Tools = require('oas3-tools');
 var serverPort = 8080;
-
+const prepareElasticsearch = require('./service/individualServices/ElasticsearchPreparation');
 var appCommons = require('onf-core-model-ap/applicationPattern/commons/AppCommons');
+
+// uncomment if you do not want to validate security e.g. operation-key, basic auth, etc
+appCommons.openApiValidatorOptions.validateSecurity = false;
 
 // swaggerRouter configuration
 var options = {
     routing: {
         controllers: path.join(__dirname, './controllers')
     },
-    //openApiValidator: appCommons.openApiValidatorOptions
+    openApiValidator: appCommons.openApiValidatorOptions
     };
 
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
-
-
-global.databasePath ='./database/load.json'
-
 appCommons.setupExpressApp(app);
 
-// Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-});
+global.databasePath ='d:/Working/VSCode/MacAddressTableRecorder/server/database/load.json'
+
+prepareElasticsearch().catch(err => {
+    console.error(`Error preparing Elasticsearch : ${err}`);
+}).finally(
+    () => {
+        // Initialize the Swagger middleware
+        http.createServer(app).listen(serverPort, function () {
+        console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+        console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+        });
+        appCommons.performApplicationRegistration();
+    }
+);
